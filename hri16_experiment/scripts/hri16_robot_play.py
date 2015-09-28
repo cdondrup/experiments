e3456e3b6d6f940c7a9af6a34ea6b85794474a31
Christian Dondrup <tkrajnik@lincoln.ac.uk>
2015-09-29 00:17:07 +0100

    .

--- hri16_experiment/scripts/hri16_robot_play.py
+++ hri16_experiment/scripts/hri16_robot_play.py
@@ -187,7 +187,7 @@ class Test(object):
 
     def button_callback(self, msg):
         print "Button pressed"
-        self.stop_times.append([])
+        if msg.B: self.stop_times.append([])
         self.b = msg.B
 
         if msg.A:
@@ -284,7 +284,9 @@ class Test(object):
             d["travel_time"] = result.travel_time
             d["condition"] = self.conditions[self.num_trial]
             stop_times = []
+            print self.stop_times
             for e in self.stop_times:
+                print e
                 stop_times.append(e[-1] - e[0])
             d["stop_time"] = stop_times
             d["stop_num"] = len(self.stop_times)
